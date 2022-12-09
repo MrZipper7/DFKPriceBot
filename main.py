1f0f8ee5b32b474164135ec2a3133002ff0291be
MrZipper <mrzipperhots@gmail.com>
2022-12-08 19:01:55 -0500

    FASTER2

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ async def on_ready():
     logger.info(f"{client.user} Online")
     priceInfo.start()
 
-@tasks.loop(seconds=24)
+@tasks.loop(seconds=12)
 async def priceInfo():
     # JEWEL Price
     try:
@@ -66,7 +66,7 @@ async def priceInfo():
     activity_string = f"JEWEL at ${round(jewelPrice, 3)}"
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=activity_string))
 
-    await asyncio.sleep(8)
+    await asyncio.sleep(4)
 
     # CRYSTAL Price
     try:
@@ -78,7 +78,7 @@ async def priceInfo():
     activity_string = f"CRYSTAL at ${round(crystalPrice, 3)}"
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=activity_string))
 
-    await asyncio.sleep(8)
+    await asyncio.sleep(4)
 
     # JADE Price
     try:
